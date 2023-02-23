e9f18ede889944dcd682ae36e6e03c8758c94014
reodz <126093549+reodz@users.noreply.github.com>
2023-02-23 16:29:15 +0100

    routes

---
+++ api.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\EnfantController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::prefix('/auth')->group(function(){
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+});
+
+Route::group(['prefix' => '/enfant', 'middleware' => ['auth:sanctum']], function(){
+    Route::post('/add', [EnfantController::class, 'add_children']);
+});
